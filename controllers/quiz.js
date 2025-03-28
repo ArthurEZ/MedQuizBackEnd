a64c5e1a5f94593f23c9f37a0854c9b4a1b700cd
Arthur <thanagorn2005@gmail.com>
2025-03-28 17:45:12 +0700

    create quiz & category controller

--- controllers/quiz.js
+++ controllers/quiz.js
@@ -1,78 +1,76 @@
 const Quiz = require('../models/Quiz')
 
 exports.getQuizzes = async (req, res, next) => {
-    
+    try {
+        const quiz = await Quiz.find();
+        res.status(200).json({ success: true, count: quiz.length, data: quiz });
+    } 
+    catch (error) {
+        console.error(error);
+        res.status(400).json({ success: false });
+    }
 }
 
-/** 
- * @desc     Get single Massage Shop
- * @route    GET /api/v1/massage-shops/:id
- * @access   Public
-*/
-exports.getMassageShop = async (req, res, next) => {
+exports.getQuizzesByCategory = async (req, res, next) => {
     try {
-        const massageShop = await MassageShop.findById(req.params.id);
+        const quiz = await Quiz.find({ category: categoryId }).populate("category");
+        res.status(200).json({ success: true, count: quiz.length, data: quiz });
+      } catch (error) {
+        console.error("Error fetching quizzes:", error);
+        res.status(400).json({ success: false });
+      }
+}
 
-        if (!massageShop) {
+exports.getQuiz = async (req, res, next) => {
+    try {
+        const quiz = await Quiz.findById(req.params.id);
+
+        if (!quiz) {
             return res.status(400).json({ success: false });
         }
 
-        res.status(200).json({ success: true, data: massageShop });
+        res.status(200).json({ success: true, data: quiz });
+    } 
+    catch (error) {
+        console.error(error);
+        res.status(400).json({ success: false });
+    }
+}
+
+exports.createQuiz = async (req, res, next) => {
+    try {
+        const quiz = await Quiz.create(req.body);
+
+        res.status(201).json({ success: true, data: quiz });
     } catch (error) {
         console.error(error);
         res.status(400).json({ success: false });
     }
 }
 
-/** 
- * @desc     Create new Massage Shop
- * @route    POST /api/v1/massage-shops
- * @access   Private
-*/
-exports.createMassageShop = async (req, res, next) => {
+exports.updateQuiz = async (req, res, next) => {
     try {
-        const massageShop = await MassageShop.create(req.body);
-
-        res.status(201).json({ success: true, data: massageShop });
-    } catch (error) {
-        console.error(error);
-        res.status(400).json({ success: false });
-    }
-}
-
-/** 
- * @desc     Update Massage Shop
- * @route    PUT /api/v1/massage-shops/:id
- * @access   Private
-*/
-exports.updateMassageShop = async (req, res, next) => {
-    try {
-        const massageShop = await MassageShop.findByIdAndUpdate(req.params.id, req.body, {
+        const quiz = await Quiz.findByIdAndUpdate(req.params.id, req.body, {
             new: true,
             runValidators: true
         });
 
-        if (!massageShop) {
+        if (!quiz) {
             return res.status(400).json({ success: false });
         }
 
-        res.status(200).json({ success: true, data: massageShop });
+        res.status(200).json({ success: true, data: quiz });
     } catch (error) {
         console.error(error);
         res.status(400).json({ success: false });
     }
 };
 
-/** 
- * @desc     Delete Massage Shop
- * @route    PUT /api/v1/massage-shops/:id
- * @access   Private
-*/
-exports.deleteMassageShop = async (req, res, next) => {
+exports.deleteQuiz = async (req, res, next) => {
     try {
-        const massageShop = await MassageShop.findByIdAndDelete(req.params.id);
+        const quiz = await Quiz.findByIdAndDelete(req.params.id);
 
-        if (!massageShop) {
+        if (!quiz) {
             return res.status(400).json({ success: false });
         }
 
